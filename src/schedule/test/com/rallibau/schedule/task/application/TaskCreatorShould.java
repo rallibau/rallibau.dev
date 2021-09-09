2fbe5ad63347c83017e15d23bb8189d8b729aa8b
ramon.llinares <rallibau@gmail.com>
2021-09-09 13:21:56 +0200

    añadimos taskcreator

---
+++ src/schedule/test/com/rallibau/schedule/task/application/TaskCreatorShould.java
@@ -0,0 +1,30 @@
+package com.rallibau.schedule.task.application;
+
+import com.rallibau.schedule.task.domain.Task;
+import com.rallibau.schedule.task.domain.TaskMother;
+import com.rallibau.schedule.task.domain.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class TaskCreatorShould {
+
+    private TaskCreator taskCreator;
+    private TaskRepository taskRepository;
+
+    @BeforeEach
+    private void setUp() {
+        this.taskRepository = mock(TaskRepository.class);
+        this.taskCreator = new TaskCreator(taskRepository);
+
+    }
+
+    @Test
+    public void create_scheduler_do_not_failure() {
+        Task task = TaskMother.random();
+        taskCreator.create(task);
+        verify(taskRepository, atLeastOnce()).save(task);
+    }
+
+}
