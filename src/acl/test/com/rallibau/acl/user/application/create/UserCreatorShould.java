a91eb4e5e56df4cce5b7b1db00c8df5074567043
ramon.llinares <rallibau@gmail.com>
2021-10-07 09:18:47 +0200

    fix

--- src/acl/test/com/rallibau/acl/user/application/create/UserCreatorShould.java
+++ src/acl/test/com/rallibau/acl/user/application/create/UserCreatorShould.java
@@ -2,10 +2,12 @@ package com.rallibau.acl.user.application.create;
 
 import com.rallibau.acl.user.domain.User;
 import com.rallibau.acl.user.domain.UserMother;
+import com.rallibau.acl.user.domain.UserPasswordMother;
 import com.rallibau.acl.user.domain.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.*;
 
 public class UserCreatorShould {
@@ -24,5 +26,14 @@ public class UserCreatorShould {
         User user = UserMother.random();
         userCreator.create(user);
         verify(repository, atLeastOnce()).save(user);
+    }
+
+    @Test
+    public void when_creat_a_user_the_password_is_encripted() {
+        User user = UserMother.random();
+        userCreator.create(user);
+        verify(repository, atLeastOnce()).save(user);
+        assertThat("the passsword not is encripted", UserPasswordMother.PASSWORD_ENCRYPTED.equals(user.userPassword().value()));
+
     }
 }
--- src/acl/test/com/rallibau/acl/user/domain/UserPasswordMother.java
+++ src/acl/test/com/rallibau/acl/user/domain/UserPasswordMother.java
@@ -1,13 +1,23 @@
 package com.rallibau.acl.user.domain;
 
 import com.rallibau.shared.domain.WordMother;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class UserPasswordMother {
+    public static final String PASSWORD_ENCRYPTED = "password_encrypted";
+
     public static UserPassword random() {
         return UserPasswordMother.create(WordMother.random());
     }
 
     private static UserPassword create(String passsword) {
-        return UserPassword.create(passsword);
+        BCryptPasswordEncoder bCryptPasswordEncoder =  mock(BCryptPasswordEncoder.class);
+        when(bCryptPasswordEncoder.encode(anyString())).thenReturn(PASSWORD_ENCRYPTED);
+        return UserPassword.create(bCryptPasswordEncoder,passsword);
     }
 }
+
