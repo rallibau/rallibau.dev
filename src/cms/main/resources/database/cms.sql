4ea660cd62ee63bf19fc8e93b6309f3526e9b035
ramon.llinares <rallibau@gmail.com>
2021-10-25 17:05:36 +0200

    add creation date to page

--- src/cms/main/resources/database/cms.sql
+++ src/cms/main/resources/database/cms.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `page` (
     `id` CHAR(36) NOT NULL,
+    `page_creation_date` DATETIME NOT NULL,
     `page_id_user` CHAR(36) NOT NULL,
     `page_title` VARCHAR(255) NOT NULL,
     `page_body` MEDIUMBLOB NOT NULL,
--- src/cms/test/com/rallibau/cms/page/domain/PageMother.java
+++ src/cms/test/com/rallibau/cms/page/domain/PageMother.java
@@ -4,12 +4,14 @@ import com.rallibau.cms.user.domain.User;
 import com.rallibau.cms.user.domain.UserMother;
 
 public final class PageMother {
-    public static Page create(PageId id, User creator, PageTitle pageTitle, PageBody pageBody) {
-        return Page.create(id, creator, pageTitle, pageBody);
+    public static Page create(PageId id, PageCreationDate pageCreationDate,User creator, PageTitle pageTitle, PageBody pageBody) {
+        return Page.create(id, pageCreationDate,creator, pageTitle, pageBody);
     }
 
     public static Page random() {
-        return Page.create(PageIdMother.random(),
+        return Page.create(
+                PageIdMother.random(),
+                PageCreationDateMother.random(),
                 UserMother.random(),
                 PageTitleMother.random(),
                 PageBodyMother.random());
