5714899f210147ab7e89df69819c2c9975fddc1b
rallibau <rallibau@gmail.com>
2021-10-28 16:29:35 +0200

    iduser creator page

--- src/cms/main/com/rallibau/cms/page/application/find/FindPageQueryHandler.java
+++ src/cms/main/com/rallibau/cms/page/application/find/FindPageQueryHandler.java
@@ -1,6 +1,8 @@
 package com.rallibau.cms.page.application.find;
 
 import com.rallibau.cms.page.domain.Page;
+import com.rallibau.cms.user.application.find.UserCmsFinder;
+import com.rallibau.cms.user.domain.User;
 import com.rallibau.shared.domain.Service;
 import com.rallibau.shared.domain.bus.query.QueryHandler;
 
@@ -8,18 +10,22 @@ import com.rallibau.shared.domain.bus.query.QueryHandler;
 public class FindPageQueryHandler implements QueryHandler<PageFinderQuery, PageResponse> {
 
     private final PageFinder pageFinder;
+    private final UserCmsFinder userCmsFinder;
 
-    public FindPageQueryHandler(PageFinder pageFinder) {
+    public FindPageQueryHandler(PageFinder pageFinder,
+                                UserCmsFinder userCmsFinder) {
         this.pageFinder = pageFinder;
+        this.userCmsFinder = userCmsFinder;
     }
 
     @Override
     public PageResponse handle(PageFinderQuery query) {
         Page page = pageFinder.find(query.pageId());
+        User user = userCmsFinder.find(page.pageIdUser().value());
         return new PageResponse(page.id().value(),
                 page.pageTitle().value(),
                 page.pageBody().value(),
-                page.getCreator().userName().value()
+                user.userName().value()
         );
     }
 }
